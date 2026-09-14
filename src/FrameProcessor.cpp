73d476a11299c44848ce59b18a5a25a60ef785ee
cristi-poli <cristi.sitea@yahoo.com>
2026-09-15 01:26:46 +0300

    Implement contour and bounding box detection

--- src/FrameProcessor.cpp
+++ src/FrameProcessor.cpp
@@ -1,8 +1,16 @@
 #include "FrameProcessor.hpp"
 
+#include <vector>
+
 cv::Mat FrameProcessor::process(const cv::Mat& frame)
 {
-    return detectEdges(frame);
+    cv::Mat output = frame.clone();
+
+    cv::Mat edges = detectEdges(frame);
+
+    detectContours(output, edges);
+
+    return output;
 }
 
 cv::Mat FrameProcessor::detectEdges(const cv::Mat& frame)
@@ -28,4 +36,36 @@ cv::Mat FrameProcessor::detectEdges(const cv::Mat& frame)
     );
 
     return edges;
+}
+
+void FrameProcessor::detectContours(
+    cv::Mat& frame,
+    const cv::Mat& edges
+)
+{
+    std::vector<std::vector<cv::Point>> contours;
+
+    cv::findContours(
+        edges,
+        contours,
+        cv::RETR_EXTERNAL,
+        cv::CHAIN_APPROX_SIMPLE
+    );
+
+    for (const auto& contour : contours)
+    {
+        double area = cv::contourArea(contour);
+
+        if (area > 500)
+        {
+            cv::Rect boundingBox = cv::boundingRect(contour);
+
+            cv::rectangle(
+                frame,
+                boundingBox,
+                cv::Scalar(0, 255, 0),
+                2
+            );
+        }
+    }
 }
